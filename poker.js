a9d26ba3f751b338f1b194ec7025b9c7bb45da75
Sergi Juanati <sergijuanati@iMac-de-Sergi.local>
2019-11-09 17:53:13 +0100

    Comprovació de combinacions

--- poker.js
+++ poker.js
@@ -1,76 +1,147 @@
+/*
+const pal = ['piques', 'cors', 'trevols', 'diamants'];
+const valor = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+*/
 
 const pal = ['piques', 'cors', 'trevols', 'diamants'];
-const valor = ['as', 'dos', 'tres', 'quatre', 'cinc', 'sis', 'set', 'vuit', 'nou', 'deu', 'sota', 'cavall', 'rei'];
-const totalMin = 1;
-let totalMax = 0;
+const valor = ['Q','K','A'];
+
+//const totalMin = 1;
+//let totalMax = 0;
 let baralla = [];
 let barrallaBarrejada = [];
 let jugadors = [];
 let jugadorsMa = [];
 
-function valorPunts(carta) {
-    switch (carta) {
-        case 'dos': return 1;
-        case 'tres': return 2;
-        case 'quatre': return 3;
-        case 'cinc': return 4;
-        case 'sis': return 5;
-        case 'set': return 6;
-        case 'vuit': return 7;
-        case 'nou': return 8;
-        case 'deu': return 9;
-        case 'sota': return 10;
-        case 'cavall': return 11;
-        case 'rei': return 12;
-        case 'as': return 13;
-        default: return 0;
-    }
-}
-
 function generaBaralla() {
+
     for (let i=0; i<pal.length; i++) {
         for (let j=0; j<valor.length; j++) {
             baralla.push({pal: pal[i], valor: valor[j]});
-            console.log("valor: ",valor[j]," score: ",valorPunts(valor[j]));
         }
     }
-    totalMax = baralla.length;
 }
 
-function generaNumAleatori() {
+function generaNumAleatori(totalMin, totalMax) {
+
     return Math.floor(Math.random() * (totalMax - totalMin + 1)) + totalMin;
 }
 
 function barrejaBaralla() {
-    for (let i=0; i<baralla.length; i++) {
-        let indexAleatori = generaNumAleatori();
+
+    let totalMax = baralla.length;
+    let currentMax = totalMax;
+    for (let i=0; i<totalMax; i++) {
+        let indexAleatori = generaNumAleatori(0,currentMax-1);
         barrallaBarrejada.push(baralla[indexAleatori]);
+        baralla.splice(indexAleatori,1);
+        currentMax--;
     }
-    //console.log(barrallaBarrejada);
 }
 
 function generaJugadors(numJugadors) {
+
     for (let i=1; i<=numJugadors; i++) {
         jugadors.push(`jugador${i}`);
     }
-    //console.log(jugadors);
 }
 
 function reparteixCartes() {
+    
     let ma = [];
     for (let i=0; i<jugadors.length; i++) {
         for (let j=0; j<5; j++) {
-            let indexAleatori = generaNumAleatori();
-            ma.push(barrallaBarrejada[indexAleatori]);
-            barrallaBarrejada.splice(indexAleatori,1);
-            totalMax--;
-
+            ma.push(barrallaBarrejada.pop());
         }
         jugadorsMa.push({jugador: jugadors[i], ma: ma });
         ma = [];
         console.log(jugadorsMa[i]);
     }
-    
+}
+
+// valor de la carta
+function comprovaValor(carta) {
+    switch (carta) {
+        case '2':   return 1;
+        case '3':   return 2;
+        case '4':   return 3;
+        case '5':   return 4;
+        case '6':   return 5;
+        case '7':   return 6;
+        case '8':   return 7;
+        case '9':   return 8;
+        case '10':  return 9;
+        case 'J':   return 10;
+        case 'Q':   return 11;
+        case 'K':   return 12;
+        case 'A':   return 13;
+        default:    return 0;
+    }
+}
+
+/*
+    Parella: 2 cartes mateix valor, 3 cartes valors diferents
+    Doble Parella: 2 cartes mateix valor + 2 cartes mateix valor, 1 carta diferent
+    Poker: 4 cartes iguals, 1 carta diferent
+    Full: 3 cartes mateix valor + 2 cartes mateix valor
+*/
+function comprovaParella(jugador, ma) {
+
+    //let parella = false;
+    let comptador = 0;
+    let j=0;
+
+    for (let i=0; i<=ma.length; i++) {
+        j=i+1;
+        while (j < ma.length) {
+            if (ma[i].valor === ma[j].valor) {
+                //parella = true;
+                comptador++;
+            }/*
+            if ((comptador === 3) && (i<=1)) {
+                console.log(jugador, "Poker: ", true);
+                return;
+            }*/
+            j++;
+        }
+    }
+
+    if (parella && (comptador === 1)) {
+        console.log(jugador, "Parella: ", true);
+    } else if (parella && (comptador === 2)) {
+        console.log(jugador, "Doble Parella: ", true);
+    } else if (parella && (comptador === 3)) {
+        console.log(jugador, "Full: ", true);
+    } else if (comptador === 4) {
+        console.log(jugador, "Full: ", true);
+    }
+}
+
+// 3 cartes mateix valor, 2 cartes diferents
+function comprovaTrio() {
+
+}
+
+// 5 cartes consecutives mateix valor
+function comprovaEscala() {
+
+}
+
+// 5 cartes mateix pal
+function comprovaColor() {
+
+}
+
+
+// 5 cartes consecutives mateix pal
+function comprovaEscalaColor() {
+
+}
+
+function comprovaMans() {
+    for (let i=0; i<jugadorsMa.length; i++) {
+        comprovaParella(jugadorsMa[i].jugador, jugadorsMa[i].ma);
+    }
 }
 
 generaBaralla();
@@ -78,3 +149,6 @@ generaNumAleatori();
 barrejaBaralla();
 generaJugadors(2);
 reparteixCartes();
+comprovaMans();
+
+//console.log(jugadorsMa[0].ma[3].valor);
