adbec26f89442e780fc0f26e6c28eb6131886688
Sergi Juanati <sergijuanati@iMac-de-Sergi.local>
2019-11-10 00:00:09 +0100

    Escala, Escala Color i Color OK

--- poker.js
+++ poker.js
@@ -1,10 +1,14 @@
-/*
-const pal = ['piques', 'cors', 'trevols', 'diamants'];
-const valor = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
-*/
 
 const pal = ['piques', 'cors', 'trevols', 'diamants'];
+const valor = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+
+/*
+const pal = ['piques', 'cors', 'trevols', 'diamants'];
 const valor = ['Q','K','A'];
+
+const pal = ['piques'];
+const valor = ['Q','K','A','J','9','8'];
+*/
 
 //const totalMin = 1;
 //let totalMax = 0;
@@ -70,11 +74,10 @@ function comprovaValor(carta) {
         case '7':   return 6;
         case '8':   return 7;
         case '9':   return 8;
-        case '10':  return 9;
-        case 'J':   return 10;
-        case 'Q':   return 11;
-        case 'K':   return 12;
-        case 'A':   return 13;
+        case 'J':   return 9;
+        case 'Q':   return 10;
+        case 'K':   return 11;
+        case 'A':   return 12;
         default:    return 0;
     }
 }
@@ -82,107 +85,81 @@ function comprovaValor(carta) {
 /*
     Parella: 2 cartes mateix valor, 3 cartes valors diferents
     Doble Parella: 2 cartes mateix valor + 2 cartes mateix valor, 1 carta diferent
+    Trio: 3 cartes mateix valor, 2 cartes diferents
     Poker: 4 cartes iguals, 1 carta diferent
     Full: 3 cartes mateix valor + 2 cartes mateix valor
+    Escala: 5 cartes consecutives mateix valor
+    Color: 5 cartes mateix pal
+    Escala Color: 5 cartes consecutives mateix pal
 */
 function comprovaCombinacions(jugador, ma) {
-/*
-    let comptador = 0;
-    let j=0;
 
-    for (let i=0; i<=ma.length; i++) {
-        j=i+1;
-        while (j < ma.length) {
-            if (ma[i].valor === ma[j].valor) {
-                comptador++;
-            }
-            j++;
-        }
-    }
-    if (comptador === 1) {
-        console.log(jugador, "Parella: ", true);
-    } else if (comptador === 2) {
-        console.log(jugador, "Doble Parella: ", true);
-    } else if (comptador === 3) {
-        console.log(jugador, "Full: ", true);
-    } else if (comptador === 4) {
-        console.log(jugador, "Poker: ", true);
-    }
-*/
-
-/*
-    var resultat = {};
-    valors.forEach(function(element) {
-        resultat[element] = (resultat[element] || 0) + 1;
-    });
-
-    console.log("Resultat:",resultat)
-    for (var element in resultat) {
-        console.log(element + ' = ' + resultat[element]);
-    } */
-
-
-    let valors = [];
+    // Passa propietats de valors i pals a arrays
+    let valors = [], pals = [], valorsEscala = [];
     for (let i=0; i<ma.length; i++) {
         valors.push(ma[i].valor);
+        pals.push(ma[i].pal);
+        valorsEscala.push(comprovaValor(ma[i].valor));
     }
 
-    // Genera array amb cada valor i el seu número de repeticions
-    let resultat = {};
-    valors.forEach(function(x) { resultat[x] = (resultat[x] || 0) + 1; });
-    //console.log(resultat);
+    // Calcula número de repeticions per cada valor / pal
+    let resultatValors = {}, resultatPals = {};
+    valors.forEach(function(x) { resultatValors[x] = (resultatValors[x] || 0) + 1; });
+    pals.forEach(function(x) { resultatPals[x] = (resultatPals[x] || 0) + 1; });
 
     // Count del número les propietats
-    let total = Object.keys(resultat).length;
+    let totalValors = Object.keys(resultatValors).length;
+    let totalPals = Object.keys(resultatPals).length;
 
-    // EMPAT O NO EMPAT? preguntar-ho.
-    for(key in resultat) {
-        if(resultat.hasOwnProperty(key)) {
-            repeticions = resultat[key];
-            if ((total === 2) && ((repeticions === 3) || (repeticions === 2))) {
+    // Comprova: Parella, Doble Parella, Trio, Full, Poker
+    let repeticions = 0;
+    for(key in resultatValors) {
+        if(resultatValors.hasOwnProperty(key)) {
+            repeticions = resultatValors[key];
+            if ((totalValors === 2) && ((repeticions === 3) || (repeticions === 2))) {
                 console.log(jugador,"-> Full");
                 return;
-            } else if ((total === 2) && ((repeticions === 4) || (repeticions === 1))) {
+            } else if ((totalValors === 2) && ((repeticions === 4) || (repeticions === 1))) {
                 console.log(jugador,"-> Poker");
                 return;
-            } else if ((total === 3) && (repeticions === 3)) {
+            } else if ((totalValors === 3) && (repeticions === 3)) {
                 console.log(jugador,"-> Trio");
                 return;
-            } else if ((total === 3) && (repeticions === 2)) {
+            } else if ((totalValors === 3) && (repeticions === 2)) {
                 console.log(jugador,"-> Doble Parella");
                 return;
-            } else if (total === 4) {
+            } else if (totalValors === 4) {
                 console.log(jugador,"-> Parella");
                 return;
-            } else if (total === 5) {
-                console.log(jugador,"-> Res de res");
-                return;
-            }
+            } 
         }
     }
 
+    // Ordena valors
+    valorsEscala.sort(function(a, b){return a - b});
 
+    // Comprova: Escala
+    let escala = true;
+    for (let i=0; i<valorsEscala.length-1; i++) {
+        if (valorsEscala[i] !== (valorsEscala[i+1]-1)) {
+            escala = false;
+        }
+    }
 
-
-  
-
-
-
-}
-
-// 3 cartes mateix valor, 2 cartes diferents
-function comprovaTrio() {
-}
-// 5 cartes consecutives mateix valor
-function comprovaEscala() {
-}
-// 5 cartes mateix pal
-function comprovaColor() {
-
-}
-// 5 cartes consecutives mateix pal
-function comprovaEscalaColor() {
-
+    // Comprova: Color
+    if ((totalPals === 1) && escala) {
+        console.log(jugador,"-> Escala color ;)");
+        return;
+    } else if (totalPals === 1) {
+        console.log(jugador,"-> Color");
+        return;
+    } else if (escala) {
+        console.log(jugador,"-> Escala");
+        return;
+    } else {
+        console.log(jugador,"-> Res de res");
+        return;
+    }
 }
 
 function comprovaMans() {
@@ -194,7 +171,7 @@ function comprovaMans() {
 generaBaralla();
 generaNumAleatori();
 barrejaBaralla();
-generaJugadors(2);
+generaJugadors(3);
 reparteixCartes();
 comprovaMans();
 
